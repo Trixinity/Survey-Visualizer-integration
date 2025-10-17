0a0765afdfb230922a20350a4a16478eaceb6a65
Bill <billvi2002@hotmail.nl>
2025-10-17 02:09:34 +0200

    Added hover color to the button component, added h-fit, bottom margin and text center to card component and removed font sans and mono.

--- survey-visualiser-integration/src/components/button.tsx
+++ survey-visualiser-integration/src/components/button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 export default function Button({ text }: ButtonProps) {
   return (
-    <div className="rounded-lg p-2 text-center bg-button">
+    <div className="rounded-lg p-2 text-center bg-button hover:bg-button-hover">
       <p className="!text-white">{text}</p>
     </div>
   );
--- survey-visualiser-integration/src/components/card.tsx
+++ survey-visualiser-integration/src/components/card.tsx
@@ -1,12 +1,12 @@
 interface CardProps {
-  title: string;
+  title?: string;
   children?: React.ReactNode;
 }
 
 export default function Card({ title, children }: CardProps) {
   return (
-    <div className="bg-card rounded-lg p-6">
-      <h3 className="font-semibold mb-4">{title}</h3>
+    <div className="bg-card rounded-lg p-6 h-fit">
+      <h3 className="font-semibold text-center mb-3">{title}</h3>
       {children}
     </div>
   );
