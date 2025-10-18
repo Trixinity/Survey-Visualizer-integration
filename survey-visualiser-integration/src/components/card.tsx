62404ffeb942adb7f7f46704950a8ecf774f8eb5
Bill <billvi2002@hotmail.nl>
2025-10-19 01:07:27 +0200

    Updated card such that it supports subtitles.

--- survey-visualiser-integration/src/components/card.tsx
+++ survey-visualiser-integration/src/components/card.tsx
@@ -1,12 +1,14 @@
 interface CardProps {
   title?: string;
+  subtitle?: string;
   children?: React.ReactNode;
 }
 
-export default function Card({ title, children }: CardProps) {
+export default function Card({ title, subtitle, children }: CardProps) {
   return (
     <div className="bg-card rounded-lg p-6 h-fit">
       <h3 className="font-semibold text-center mb-3">{title}</h3>
+      <p className="text-center">{subtitle}</p>
       {children}
     </div>
   );
