13a8e856d9c3fe881ff4794d842d825cd87542b4
Bill <billvi2002@hotmail.nl>
2025-10-19 02:04:51 +0200

    Implemented a function to count occurences by category of difficulty in questions.

--- survey-visualiser-integration/src/hooks/questionsApi.ts
+++ survey-visualiser-integration/src/hooks/questionsApi.ts
@@ -1,4 +1,4 @@
-import { apiRequest } from './apiClient';
+import { apiRequest, decodeHtmlEntities } from './apiClient';
 
 interface ApiResponse {
   response_code: number;
@@ -13,10 +13,40 @@ export interface Questions {
   incorrect_answers: string[];
 }
 
-/*
-  Only supports the GET endpoint from Open Trivia DB.
-  Can be extended in the future if needed.
+/**
+ * Only supports the GET endpoint from Open Trivia DB.
+ * Can be extended in the future if needed.
 */
 export const openTriviaDB = {
   getOpenTriviaQuestions: () => apiRequest<ApiResponse>()
+}
+
+/**
+ * Process questions data to count occurrences by category or difficulty.
+ * @param questions.
+ * @param type. Category or difficulty.
+ * @returns Array of objects with name and count properties.
+ */
+export function processQuestionsData(questions: Questions[], type: string) {
+  const categoryCount = questions.reduce((acc, question) => {
+    let decodedCategory;
+    switch (type) {
+      case ('category'):
+        decodedCategory = decodeHtmlEntities(question.category);
+        break;
+      case ('difficulty'):
+        decodedCategory = decodeHtmlEntities(question.difficulty);
+        break;
+      default:
+        decodedCategory = 'category';
+        break;
+    }
+    acc[decodedCategory] = (acc[decodedCategory] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
+  return Object.entries(categoryCount).map(([category, count]) => ({
+    name: category,
+    count: count
+  }));
 }
